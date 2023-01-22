8574eb8423b266e28b22f235190ddc516cde9327
sepydev <mrprocs@gmail.com>
2023-01-23 02:22:14 +0330

    Update stop loss and profit

---
+++ kucoin_operations/update_stop_loss.py
@@ -0,0 +1,18 @@
+import kucoin
+
+from kucoin_operations.operation_inteface import IOperation
+
+
+class UpdateStopLoss(IOperation):
+    def validate(self, **kwargs):
+        order_id = kwargs.get('order_id')
+        stop_price = kwargs.get('stop_price')
+        if any(map(lambda x: x is not None, [order_id, stop_price])):
+            raise ValueError("Missing argument")
+        return True
+
+    def execute(self, kucoin_client: kucoin.client.Market, trade_client: kucoin.client.Trade, **kwargs):
+        if self.validate(**kwargs):
+            order_id = kwargs.get('order_id')
+            stop_price = kwargs.get('stop_price')
+            return kucoin_client.update_stop_order(order_id, stop_price)
