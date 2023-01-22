1b5714e641bca20ca4d9104a98e25370cafe55ff
sepydev <mrprocs@gmail.com>
2023-01-23 02:22:27 +0330

    Cancel stop loss and profit

---
+++ kucoin_operations/cancel_take_profit.py
@@ -0,0 +1,16 @@
+import kucoin
+
+from kucoin_operations.operation_inteface import IOperation
+
+
+class CancelTakeProfit(IOperation):
+    def validate(self, **kwargs):
+        order_id = kwargs.get('order_id')
+        if any(map(lambda x: x is not None, [order_id])):
+            raise ValueError("Missing argument")
+        return True
+
+    def execute(self, kucoin_client: kucoin.client.Market, trade_client: kucoin.client.Trade, **kwargs):
+        if self.validate(**kwargs):
+            order_id = kwargs.get('order_id')
+            return kucoin_client.cancel_take_profit_order(order_id)
